817bf1c500bd4b3dd77e5a6bd7c2c4c917754153
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2022-10-22 22:29:09 -0300

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,7 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(sorted(linguagens, key = lambda x:len(x))) # ['c', 'js', 'java', 'python', 'csharp']
+
+print(sorted(linguagens, key = lambda x:len(x), reverse = True)) # ['python', 'csharp', 'java', 'js', 'c']
+
+print(sorted(linguagens))
