a6fe3116e9577538cb869280c2f1fac7c7b506ac
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2022-10-24 06:25:52 -0300

    Add files via upload

---
+++ declarando_dicionario.py
@@ -0,0 +1,13 @@
+pessoa = {"nome": "Guilherme", "idade": 28}
+print(pessoa)
+
+# mesmo resultado das duas maneiras
+
+pessoa = dict(nome = "Guilherme", idade = 28)
+print(pessoa)
+
+pessoa["telefone"] = "3333-1234"
+print(pessoa)
+
+pessoa["nome"] = "Maria"
+print(pessoa)
