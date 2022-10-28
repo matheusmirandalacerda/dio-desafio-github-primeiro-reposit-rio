0ed18071347a2208d68b54705a88d92a64c65c5b
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2022-10-27 23:35:32 -0300

    Add files via upload

---
+++ desafio_bicicletaria.py
@@ -0,0 +1,40 @@
+class Bicicleta:
+    def __init__(self,cor,modelo,ano,valor,aro = 18):
+        self.cor = cor 
+        self.modelo = modelo 
+        self.ano = ano 
+        self.valor = valor
+        self.aro = aro
+
+    def buzinar(self):
+        print("Plim plim...")
+
+    def parar(self):
+        print("Parando bicicleta...")
+        print("Bicicleta parada")
+
+    def correr(self):
+        print("Vrummmmmm...")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+b1 = Bicicleta("vermelha", "caloi", 2022, 600) 
+b1.buzinar()
+b1.correr()
+b1.parar()
+print(b1.cor,b1.modelo,b1.ano,b1.valor)
+
+
+
+b2 = Bicicleta("verde","monark",2000,189) 
+b2.buzinar()
+# maneiras diferente de chamar o método
+Bicicleta.buzinar(b2)
+print(b2.cor)
+print(b2)
+
+
+    
+
