e113d4a1840f3e24ae7143088bb1b7c88f915589
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2022-10-29 06:30:41 -0300

    Add files via upload

---
+++ metodos_classe_estatico.py
@@ -0,0 +1,26 @@
+class Pessoa:
+    def __init__(self, nome = None , idade = None):
+        self.nome = nome 
+        self.idade = idade 
+
+
+
+    @classmethod
+    def criar_de_data_nascimento(cls,ano,mes, dia,nome):
+        print(cls)
+        idade = 2022 - ano
+        return cls(nome, idade)
+
+    @staticmethod
+    def e_maior_idade(idade):
+        return idade >= 18
+
+
+# p = Pessoa("Guilherme", 28)
+# print(p.nome, p.idade)
+
+p = Pessoa.criar_de_data_nascimento(1994,3,21,"Guilherme")
+print(p.nome,p.idade)
+
+print(Pessoa.e_maior_idade(18))
+print(Pessoa.e_maior_idade(8))
